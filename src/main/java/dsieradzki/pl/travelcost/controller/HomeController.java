1f6f116515f26b8e18f521bf7b4e207055f23017
DSieradzki <daw.sieradzki@gmail.com>
2021-06-27 12:04:57 +0200

    add homepage

--- src/main/java/dsieradzki/pl/travelcost/controller/HomeController.java
+++ src/main/java/dsieradzki/pl/travelcost/controller/HomeController.java
@@ -1,16 +1,20 @@
 package dsieradzki.pl.travelcost.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
 public class HomeController {
     @Value("${travelcost}")
     String appName;
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String homePage(Model model) {
         model.addAttribute("appName", appName);
         return "index";
